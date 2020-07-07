c32f9613fe2bb49e7507fc3ad60e0f599c0e0158
vagrant <vagrant@homestead>
2020-07-07 14:27:15 +0000

    add migration to order on question and question form

---
+++ database/migrations/2020_07_07_131905_order_question_on__relation.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OrderQuestionOnRelation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('inclusive_form_questions', function (Blueprint $table) {
+    
+            $table->integer('orden')->nullable();
+
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('inclusive_form_questions', function (Blueprint $table) {
+            $table->dropColumn('orden');
+             
+            });
+    }
+}

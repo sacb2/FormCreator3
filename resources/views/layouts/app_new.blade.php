485042efbe5e210313d3da0d25aa4442fcf75771
vagrant <vagrant@homestead>
2020-07-14 19:42:39 +0000

    fix on menú

--- resources/views/layouts/app_new.blade.php
+++ resources/views/layouts/app_new.blade.php
@@ -1,55 +1,75 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <!doctype html>
-  
-
-   
-<head>
+<html lang="en">
+    <style>
+    
+        @if(!isset($style_font)||$style_font==null)
+            @php
+                $style_font='1';
+                $size='1em';    
+            @endphp
+        @endif
+        @if(!isset($style_color)||$style_color==null)
+            @php
+                $style_color='4';
+                $bcolor='#fff';
+                $color='#636b6f';
+            @endphp
+        @endif
+        
+        </style> 
+        <style>
+        #outer {
+            width: 100%;
+            text-align: center;
+          }
+          
+          #inner {
+            display: inline-block;
+          }
+          </style>
+  <head>
+  	<title> Postulaciones</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!--fontAwesom -->
     <script src="https://kit.fontawesome.com/39806f2671.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  </head>
+  <body>
+		
+		<div class="wrapper d-flex align-items-stretch">
+	<!--		<nav id="sidebar" class="active"> -->
+     <!-- Menú Lateral Izquierdo-->
+    	<!--</nav> -->
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Page Content  -->
+      <div id="content" class="p-4 p-md-5">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
 
-    <title>Formulario de postulación</title>
+           
+            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Karma" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app_new.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Postulación
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-                    
-                    <div id="outer">  
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Right Side Of Navbar -->
+              <!--Accesibilidad -->
+              <div id="outer">  
                         <div id="inner">
                             <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
                                 <div class="btn-group mr-2" role="group" aria-label="First group">
                                    
-                                   <form method="POST"  action="{{ URL::current() }}" enctype="multipart/form-data">
+                                  
+                                    
+                                      
+                                  
+                                    
+                                    <form method="POST"  action="{{ URL::current() }}" enctype="multipart/form-data">
                                         {{ csrf_field() }}
                                         <button aria-hidden="true"  type="submit" name='style_font' value='1' role="button" class="btn btn-info"><i class="glyphicon glyphicon-ok-circle"></i>
                                             {{ __('Tamaño Fuente') }}
@@ -83,42 +103,33 @@
                             </div>
                     </div>
                 </div>
-            </div>
-           
-      
-                       <!-- Page Content  -->
-                  
-    
-                       <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+              <!-- Accesibilidad -->
+                <ul class="navbar-nav ml-auto">
                         
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Acceso') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
-                                </li>
-                            @endif
-                        @else
-                    <!--   <li><i class="fas fa-globe-asia">CN</i></li>
-                        <li><i class="fas fa-globe-asia">JP</i></li>
-                        <li><i class="fas fa-globe-africa">HI</i></li>
-                        <li><i class="fas fa-globe-americas">ES</i></li>
-                        <li><i class="fas fa-globe-europe">EN</i></li> -->
-                            <li class="nav-item dropdown">
-                            
-                               
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                               
+                  <!-- Authentication Links -->
+                  @guest
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('login') }}">{{ __('Acceso') }}</a>
+                      </li>
+                      @if (Route::has('register'))
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
+                          </li>
+                      @endif
+                  @else
+              <!--   <li><i class="fas fa-globe-asia">CN</i></li>
+                  <li><i class="fas fa-globe-asia">JP</i></li>
+                  <li><i class="fas fa-globe-africa">HI</i></li>
+                  <li><i class="fas fa-globe-americas">ES</i></li>
+                  <li><i class="fas fa-globe-europe">EN</i></li> -->
+                      <li class="nav-item dropdown">
+                         
+                          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                              {{ Auth::user()->name }} <span class="caret"></span>
+                          </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                               
                               <a class="dropdown-item" href="{{ route('logout') }}"
                                  onclick="event.preventDefault();
@@ -126,41 +137,37 @@
                                   {{ __('Logout') }}
                               </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                  
-                                </div>
-                            </li>
-                            @if(isset(Auth::user()->type_id))
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                  @csrf
+                              </form>
+                            
+                          </div>
+                      </li>
+                      @if(isset(Auth::user()->type_id))
                                 @if(Auth::user()->type_id==1||Auth::user()->type_id==0||Auth::user()->type_id==3||Auth::user()->type_id==2)
                                 <a class="nav-link" href="{{ route('SelectForms') }}">{{ __('Acceso Profesionales') }}</a>
                                 @endif
-                            @endif
-                        @endguest
-                    </ul>
-                 
-                       </div>
-        </nav>
-    
-
-        <main class="py-4">
-            @if(Session::get('alertSent') == "Alert")
-            <div class="alert alert-success" role="alert">
-              <strong>{{Session::get('message')}}.</strong> 
-              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
+                                @endif
+                  @endguest
+              </ul>
             </div>
-                @endif
-            @yield('content')
-          
+          </div>
+        </nav>
+<!-- 
+        <h2 class="mb-4">Sidebar #07</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ -->
+        <main class="py-4">
+             @yield('content')
         </main>
-    </div>
+      </div>
+		</div>
+
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/popper.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
 
-</body>
+  </body>
 </html>

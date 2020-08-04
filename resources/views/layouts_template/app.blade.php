adac9ff97527ed65caf2b3d4b414d23650925c4c
vagrant <vagrant@homestead>
2020-08-04 03:28:59 +0000

    add error message on view

--- resources/views/layouts_template/app.blade.php
+++ resources/views/layouts_template/app.blade.php
@@ -116,6 +116,25 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
  -->
         <main class="py-4">
+          <!-- mensaje de error -->  
+          @if($errors->any())
+          <div class="alert alert-dark alert-dismissible fade show" role="alert">
+            <h4>{{$errors->first()}}</h4>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          @endif
+          @if(Session::has('message'))
+          <div class="alert alert-dark alert-dismissible fade show" role="alert">
+            <h4>{{ Session::get('message')}}</h4>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          @endif
+          <!-- mensaje de error -->
+
              @yield('content')
         </main>
       </div>
